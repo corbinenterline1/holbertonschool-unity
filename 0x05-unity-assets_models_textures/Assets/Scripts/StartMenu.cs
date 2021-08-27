a999c4d8ede4112ae245cf3c67f3eb5f2cf8f8f3
corbinenterline1 <corbinenterline1@gmail.com>
2021-08-27 02:56:15 -0500

    0x05

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void PlayLevel()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+
+    public void QuitLevel()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
