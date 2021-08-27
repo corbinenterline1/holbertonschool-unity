7f2765ab5c0474d7b29c534ba64e42792a7e03d8
corbinenterline1 <corbinenterline1@gmail.com>
2021-08-27 02:55:44 -0500

    0x02

---
+++ 0x02-unity-scripting/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Private variable to store the offset distance between the player & camera.
+    private Vector3 offset;
+    /// <summary>
+    /// Public variable to store a reference to the player game object.
+    /// </summary>
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        // Calculate & store the offset value by getting the distance between the player's position & camera's position.
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ 0x02-unity-scripting/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayerController : MonoBehaviour
+{
+    /// <summary>
+    /// Floating point variable to store the player's movement speed.
+    /// </summary>
+    public float speed = 20;
+
+    AudioSource source;
+
+    /// <summary>
+    /// Drag for player
+    /// </summary>
+    public float dragFactor = 0.98f;
+
+    private Rigidbody playerRB;
+
+    // Start is called before the first frame update.
+    void Start()
+    {
+        // Get & store a reference to the Rigidody component so that we an access it.
+        playerRB = GetComponent<Rigidbody>();
+        // Links audiosource variable to audio source component.
+        source = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Walls")
+        {
+            source.Play();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+
+        // Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        // Use the two store floats to create a new Vector 3variable movement.
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        playerRB.velocity *= dragFactor;
+
+        // Call the AddForce function of our Rigidbody playerRB supplying movement multiplied by speed to move our player.
+        playerRB.AddForce (movement * speed);
+    }
+}
